7d24675f2927709415de7faa87547f3330797ab0
Gabriel Gustavo Dias Barreto <gabrielgdiasb@gmail.com>
2024-03-20 18:15:57 -0300

    fix generate migrations

--- dist/data-source.js
+++ dist/data-source.js
@@ -20,7 +20,7 @@ const setDataSourceConfig = () => {
             type: "postgres",
             url: process.env.DATABASE_URL,
             entities: [Products_1.Products, User_1.User],
-            migrations: ["src/migrations/*.ts"],
+            migrations: ["src/migrations/*.ts/*js"],
         };
     }
     return {
@@ -33,7 +33,7 @@ const setDataSourceConfig = () => {
         synchronize: false,
         logging: false,
         entities: [Products_1.Products, User_1.User],
-        migrations: ["src/migrations/*.ts"],
+        migrations: ["src/migrations/*.ts/*js"],
     };
 };
 const dataSourceConfig = setDataSourceConfig();
